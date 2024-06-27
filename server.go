87fd137cd059cd50bd294a6d23326afb1c74d23c
Eduardocardosodev <eduardocardoso.dev@gmail.com>
2024-06-27 14:01:55 -0300

    secrets e variaveis de ambientes

--- server.go
+++ server.go
@@ -9,6 +9,8 @@ import (
 )
 
 func main() {
+	http.HandleFunc("/secret", Secret)
+
 	http.HandleFunc("/configmap", ConfigMap)
 
 	http.HandleFunc("/", Hello)
@@ -33,4 +35,11 @@ func ConfigMap(w http.ResponseWriter, r *http.Request){
 		log.Fatalf("Error reading file: ", err)
 	}
 	fmt.Fprintf(w, "My family: %s.", string(data))
+}
+
+func Secret(w http.ResponseWriter, r *http.Request){
+
+	user:= os.Getenv("USER")
+	password := os.Getenv("PASSWORD")
+	fmt.Fprintf(w, "User: %s. Password: %s.", user, password)
 }
